f410cf594f334f4df87114959445efa149c50b73
Marshmallow009 <156039999+Marshmallow009@users.noreply.github.com>
2026-09-11 10:24:47 -1000

    Pause Menu
    still some bugs

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,12 +1,30 @@
 extends Control
 
+
+func pause():
+	get_tree().paused = true
+	show()
+	##pause_menu_buttons.show()
+
 func resume():
 	get_tree().paused = false
-	
-func pause():
-	get_tree().paused = true 
-	
+	hide()
+
 func testEsc():
-	if Input.is.action_just_pressed("ui_canceled") and get_tree().pasued == false:
-		paused()
+	if Input.is_action_just_pressed("esc") and get_tree().pasued == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
 		
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_options_pressed() -> void:
+	##pause_menu_buttons.hide()
+	pass
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()

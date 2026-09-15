aa9ed69abeaf11d2a965c1e2eab7c77be058cb29
Marshmallow009 <156039999+Marshmallow009@users.noreply.github.com>
2026-09-15 09:18:51 -1000

    Pause Menu
    buttons in pause menu work except for options which will bring up the settings menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,30 +1,27 @@
 extends Control
 
-
 func pause():
 	get_tree().paused = true
 	show()
-	##pause_menu_buttons.show()
 
 func resume():
 	get_tree().paused = false
 	hide()
 
 func testEsc():
-	if Input.is_action_just_pressed("esc") and get_tree().pasued == false:
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
 		pause()
 	elif Input.is_action_just_pressed("esc") and get_tree().paused:
 		resume()
 		
-
 func _on_resume_pressed() -> void:
 	resume()
 
-
 func _on_options_pressed() -> void:
-	##pause_menu_buttons.hide()
 	pass
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	
+func _process(delta):
+	testEsc()
